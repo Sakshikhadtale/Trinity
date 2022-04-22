a28b76b9c93aff0db1f6af8fa40b12ee5eddb87e
Your Name <sakukhadtale@gmail.com>
2022-04-22 16:55:59 +0530

    about us

--- assets/js/main.js
+++ assets/js/main.js
@@ -110,7 +110,21 @@
     }
     mainSlider();
 
-
+    function myFunction() {
+      var dots = document.getElementById("dots");
+      var moreText = document.getElementById("more");
+      var btnText = document.getElementById("myBtn");
+    
+      if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        btnText.innerHTML = "Read more"; 
+        moreText.style.display = "none";
+      } else {
+        dots.style.display = "none";
+        btnText.innerHTML = "Read less"; 
+        moreText.style.display = "inline";
+      }
+    }
 
 /* 4. Testimonial Active*/
 var testimonial = $('.h1-testimonial-active');
